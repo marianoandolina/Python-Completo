3db7ee46fc5ce85f93bac431cd8faa19f5d62290
Mariano Andolina <mariano.3gs@gmail.com>
2023-06-07 20:48:27 -0300

    bucles 2

--- Bucles/iterar_listas.py
+++ Bucles/iterar_listas.py
@@ -29,6 +29,7 @@ for animal in animales:
     # podemos hacer el print asi para que veamos cada vuelta
     print(f'Ahora la variable animal es igual a: {animal}')
 
+print('-------------------------------------')
 
 # realizamos un bucle con numeros que realiza una multiplicacion en cada vuelta
 # creamos una lista con numeros
@@ -40,3 +41,51 @@ for numero in numeros:
     resultado = numero * 10
     # mostramos por pantalla el resultado
     print(resultado)
+
+print('-------------------------------------')
+
+'''FUNCION zip()'''
+
+# podemos iterar dos listas juntas
+# para realizar esto tenemos que tener listas que contengan la misma cantidad de elementos
+
+print('Funcion zip()')
+print()
+for numero, animal in zip(numeros, animales):
+    print(f'Recorriendo lista 1: {numero}')
+    print(f'Recorriendo lista 2: {animal}')
+
+print('-------------------------------------')
+
+'''FUNCION range()'''
+
+print('Funcion range()')
+print()
+
+# si le ponemos un parametro solo a range() va desde cero hasta ese parametro
+# si le ponemos 2 parametros arranca con el primero y termina 1 numero menos que el segundo
+# si le ponemos 3 parametros, el ultimo es de cuanto en cuanto
+# para cada numero en el rango de 5 a 11 (el 11 no esta incluido) hace lo siguiente
+for num in range(5, 11):
+    # muestra el numero guardado en la variable en cada vuelta
+    print(num)
+
+print('-------------------------------------')
+
+# forma no optima de recorrer una lista
+print('Forma no optima de recorrer una lista con range()')
+print()
+
+for num in range(len(numeros)):
+    print(numeros[num])
+
+print('-------------------------------------')
+
+# forma correcta de recorrer una lista con su indice
+
+print('Forma correcta de recorrer una lista por su indice')
+print('Funcion enumerate()')
+print()
+
+for num in enumerate(numeros):
+    print(numeros[num])
