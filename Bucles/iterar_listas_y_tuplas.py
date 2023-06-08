2b5e48f0071de7ed44362affb21072e112a39f12
Mariano Andolina <mariano.3gs@gmail.com>
2023-06-08 20:33:45 -0300

    iterar conjuntos o sets

--- Bucles/iterar_listas_y_tuplas.py
+++ Bucles/iterar_listas_y_tuplas.py
@@ -87,5 +87,61 @@ print('Forma correcta de recorrer una lista por su indice')
 print('Funcion enumerate()')
 print()
 
+# este bucle genera una tupla en la variable num que contiene el indice y el dato de la variable pasada como parametro en enumerate
+# es decir que si solo le pedimos que muestre la variable num retorna una tupla num = (indice,valor) en cada iteracion
+
+print('Tupla completa')
+print('Funcion enumerate()')
+print()
+
 for num in enumerate(numeros):
-    print(numeros[num])
+    print(num)
+
+print('-------------------------------------')
+
+# si queremos acceder solo al indice de la tupla en cada iteracion
+print('Solo el index')
+print('Funcion enumerate()')
+print()
+
+for num in enumerate(numeros):
+    # en el indice 0 se encuentra el indice de cada dato
+    print(num[0])
+
+print('-------------------------------------')
+
+# si queremos acceder solo al valor de la tupla en cada iteracion
+print('Solo el valor')
+print('Funcion enumerate()')
+print()
+for num in enumerate(numeros):
+    # en el indice 1 se encuentra el valor
+    print(num[1])
+
+print('-------------------------------------')
+
+# si queremos mostrar indice y valor de la tupla
+print('Accedemos al inidice y al valor')
+print('Funcion enumerate()')
+print()
+
+for num in enumerate(numeros):
+    indice = f"El indice es {num[0]}"
+    valor = f"El valor es {num[1]}"
+    print(indice, valor)
+
+print('-------------------------------------')
+
+# usando el else en el for
+# el else se ejecuta siempre aunque el bucle no itere nada porque la lista esta vacia
+# el else se ejecuta una vez al final del bucle y no en cada vuelta
+print('Agregando el else en el bucle')
+print()
+for numero in numeros:
+    print(f"Ejecutando el ultimo bucle, valor actual: {num}")
+else:
+    print("El bucle termino")
+
+print('-------------------------------------')
+
+'''Todos los bucles funcionarian igual si reemplazamos las listas que iteramos por una tupla'''
