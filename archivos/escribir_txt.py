0f12995d33b29709fc5d23f4f10a6f1e2b3e617a
Mariano Andolina <mariano.3gs@gmail.com>
2023-06-25 11:28:44 -0300

    escribiendo en archivos txt

---
+++ archivos/escribir_txt.py
@@ -0,0 +1,9 @@
+#       ESCRIBIR ARCHIVOS TXT
+
+# abrimos el archivo, le pasamos la ruta
+# como primer parametro le indicamos que vamos a escribir en el archivo con la "w"
+# y en en el segundo parametro le indicamos como siempre la codificacion
+with open("archivos\\texto_prueba.txt","w",encoding="UTF-8") as archivo:
+
+
+
