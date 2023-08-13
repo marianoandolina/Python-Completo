e320c468db412516218cbe7d532acc29dda1f77a
Mariano Andolina <mariano.3gs@gmail.com>
2023-08-13 19:49:04 -0300

    herencias multiples

--- Poo/herencia_multiple.py
+++ Poo/herencia_multiple.py
@@ -32,11 +32,32 @@ class EmpleadoArtista(Persona, Artista):
         self.empresa = empresa
 
     # para heredar un metodo de la clase padre utilizamos super()
-    def presentarse(self):
+    # en este caso que estamos usando metodos, su usamos self nos trae el metodo de la clase actual y si usamos super() nos trae el metodo de la clase padre.
+    def presentarse_clase_padre(self):
         return f"Hola, soy {self.nombre}, {super().mostrar_habilidad()} y trabajo en {self.empresa}"
+    
+    # creamos este metodo para probar los diferentes tipos de mostrar el metodo padre y el metodo de la clase en la que estamos
+    def mostrar_habilidad(self):
+        return "No tengo habilidad ninguna, ajjajajaja"
+
+    # tenemos creado el metodo mostrar_habilidad en la clase Artista y tambien en la clase EmpleadoArtista 
+    # self:  nos muestra el metodo que tenemos creado en esta clase
+    # super():  nos muestra el metodo que tenemos creado en la clase padre
+    # creamos el metodo presentarse pero ahora mostrando el metodo mostrar_habilidad de esta clase y no de la clase padre
+
+    def presentarse(self):
+        return f"Hola, soy {self.nombre}, {self.mostrar_habilidad()} y trabajo en {self.empresa}"
+    
 
 # creamos a la presona con toda la informacion
 persona_1 = EmpleadoArtista("Mariano","40","Argentina","Guitarrista","200000","Sony")
 print(persona_1.nombre)
+
+# mostramos los llamados a metodos del mismo nombre pero de diferentes clases
+
+print(persona_1.presentarse_clase_padre())
 print(persona_1.presentarse())
+
+
+
 
