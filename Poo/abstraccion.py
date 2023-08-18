8827581343396204fa1baa182ac8a9614cc78d99
Mariano Andolina <mariano.3gs@gmail.com>
2023-08-17 21:42:45 -0300

    clases abstractas inicio

--- Poo/abstraccion.py
+++ Poo/abstraccion.py
@@ -1,3 +1,29 @@
 #       ABSTRACCION
 
-#
+#       Significa manejar la complejidad ocultando todos los detalles innecesarios al usuario o al programador y dandole solo las funcionalidades relevantes
+#       No necesitas saber como funciona internamente si no que solamente tenes que saber como usarlo
+#       Basicamente es ocultar la complejidad de un sistema
+
+# ej:
+
+
+class Auto:
+    def __init__(self):
+        self._estado = "apagado"
+
+    def encender(self):
+        print("El auto esta encendido")
+
+    def conducir(self):
+        if self._estado == "apagado":
+            self.encender()
+        print("Conduciendo el auto")
+
+
+mi_auto = Auto()
+mi_auto.conducir()
+
+"""En este ejemplo es una abstraccion porque el usuario solo aplica la funcion conducir pero no tiene idea lo que esta ocurriendo dentro de la funcion,
+por ejemplo que la funcion verifica si el auto esta encendido y de no ser asi lo enciende para empezar a conducir, el usuario solo usa la funcion y esta
+abstraido de todo lo que ocurre al momento de ejecutarla
+"""
---
+++ Poo/clases_abstractas.py
@@ -0,0 +1,5 @@
+#       CLASES ABSTRACTAS
+
+#       Una clase abstracta es una clase que no podemos instanciar pero es como una receta.
+#       Es una especie de platilla que podemos usar para otras clases, es parecido a herencia pero un poco mas complejo porque la clase abstracta no se puede instanciar como
+#       si se puede instanciar una clase padre.
