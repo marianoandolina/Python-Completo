f9d2942c7a3ac0d9114424ede4a25ccc149505e2
Mariano Andolina <mariano.3gs@gmail.com>
2023-08-19 11:16:14 -0300

    ejercicios 3 comienzo

---
+++ Poo/metodos_dunder.py
@@ -0,0 +1,12 @@
+#       METODOS ESPECIALES O DUNDER METHODS
+
+# Son los metodos que vienen por defecto en python
+# Comienzan con doble guion bajo y termina con doble guion bajo
+# Tienen funciones definidad internamente
+
+# Ej:
+#   __init__
+#   __str__
+#   __add__
+
+# Investigar y probar si queremos profundizar. 
