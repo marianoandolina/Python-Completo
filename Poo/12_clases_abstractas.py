00a91f07b188dabd87b6bc23e9997c08c9402bb2
Mariano Andolina <mariano.3gs@gmail.com>
2023-09-24 20:16:47 -0300

    revision poo terminada

--- Poo/12_clases_abstractas.py
+++ Poo/12_clases_abstractas.py
@@ -12,22 +12,28 @@ from abc import ABC, abstractclassmethod
 
 # Creamos la clase abstracta
 
+
 class Persona(ABC):
-    @abstractclassmethod # con este decorador le indicamos que la clase es abstracta
+    # con este decorador le indicamos que la clase es abstracta
+    @abstractclassmethod
     def __init__(self, nombre, edad, sexo, actividad):
         self.nombre = nombre
         self.edad = edad
         self.sexo = sexo
         self.actividad = actividad
-    
-    @abstractclassmethod # con este decorador le indicamos que el metodo es abstracto
+
+    # con este decorador le indicamos que el metodo es abstracto
+    @abstractclassmethod
     def hacer_actividad(self):
         pass
 
-    def presentarse(self): # si no le indicamos nada significa que el metododo es generico
+    def presentarse(
+        self,
+    ):  # si no le indicamos nada significa que el metododo es generico
         print(f"Hola, me llamo {self.nombre} y tengo {self.edad} años")
 
-# Al definir un metodo abstracto o un constructor le estamos indicando que al momento de crear la clase que va a heredar la abstraccion tenemos que definir si o si 
+
+# Al definir un metodo abstracto o un constructor le estamos indicando que al momento de crear la clase que va a heredar la abstraccion tenemos que definir si o si
 # estos metodos aunque sea sin ninguna implementacion de codigo para luego poder poder instanciar o de lo contrario dara error.
 # Ejemplo de cuando daria error:
 """
@@ -39,26 +45,29 @@ class Estudiante(Persona):
         super().__init__(nombre, edad, sexo, actividad)
 """
 
+
 # Creamos la clase de manera correcta agregando el metodo hacer_actividad(self)
 class Estudiante(Persona):
     def __init__(self, nombre, edad, sexo, actividad):
         super().__init__(nombre, edad, sexo, actividad)
-    
+
     def hacer_actividad(self):
         print(f"Estoy estudiando: {self.actividad}")
+
 
 # Creamos otra clase que tambien hereda de la clase abstracta Persona
 class Trabajador(Persona):
     def __init__(self, nombre, edad, sexo, actividad):
         super().__init__(nombre, edad, sexo, actividad)
-    
+
     def hacer_actividad(self):
         print(f"Actualmente estoy trabajando en el rubro de {self.actividad}")
 
-oriana = Estudiante("oriana",9,"Masculino","Primaria 4to grado")
+
+oriana = Estudiante("oriana", 9, "Masculino", "Primaria 4to grado")
 oriana.presentarse()
 oriana.hacer_actividad()
 
-mariano = Trabajador("Mariano", 40,"Masculino","Programador")
+mariano = Trabajador("Mariano", 40, "Masculino", "Programador")
 mariano.presentarse()
 mariano.hacer_actividad()
